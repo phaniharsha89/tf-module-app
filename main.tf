d4f7697ec97094db7bccf33c8b85870a3c67ab52
phaniharsha89 <phaniharsha89@gmail.com>
2023-07-27 16:21:41 +0530

    sfsd

--- main.tf
+++ main.tf
@@ -1,8 +1,107 @@
-resource "null_resource" "test" {
-    triggers = {
-        xyz=timestamp()
+resource "aws_iam_policy" "policy" {
+  name        = "${var.component}-${var.env}-ssm-pm-policy"
+  path        = "/"
+  description = "${var.component}-${var.env}-ssm-pm-policy"
+
+policy = jsonencode( {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "ssm:GetParameterHistory",
+                "ssm:GetParametersByPath",
+                "ssm:GetParameters",
+                "ssm:GetParameter"
+            ],
+            "Resource": "arn:aws:ssm:us-east-1:192494896405:parameter/roboshop.${var.env}.${var.component}.*"
+        }
+    ]
+}
+        )
+}
+
+resource "aws_iam_role" "role" {
+  name = "${var.component}-${var.env}-ec2-role"
+
+  assume_role_policy =jsonencode({
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
     }
-  provisioner "local-exec" {
-    command = "echo Hello world - Env - ${var.env}"
+  ]
+})
+}
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.component}-${var.env}-ec2-role"
+  role = aws_iam_role.role.name
+}
+
+
+resource "aws_security_group" "sg" {
+  name = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
   }
 }
+
+resource "aws_instance" "instance" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+
+  tags = {
+    Name = "${var.component}-${var.env}"
+  }
+
+}
+
+resource "aws_route53_record" "dns" {
+  zone_id = "Z08003803OC4Y6A8SK8BX"
+  name    = "${var.component}-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
+}
+
+resource "null_resource" "ansible" {
+  depends_on = [aws_instance.instance, aws_route53_record.dns]  
+  provisioner "remote-exec" {
+    
+    connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = aws_instance.instance.public_ip
+    }
+      inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/phaniharsha89/roboshop-ansible main.yml -e env=${var.env} -e role_name=${var.component}"
+      ]
+     }
+    }
+
